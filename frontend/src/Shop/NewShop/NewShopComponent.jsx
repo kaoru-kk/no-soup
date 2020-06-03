7336b65fff7735adf1d7bc8e314084a475e6dd12
19980410 <kkaoru1998@gmail.com>
2020-06-03 22:33:27 +0900

    入力フォームの完成

--- frontend/src/Shop/NewShop/NewShopComponent.jsx
+++ frontend/src/Shop/NewShop/NewShopComponent.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 
 
 
 const NewShopComponent = () => {
-    const submit = () => {
-        
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => {
         const qs = require("qs");
-        axios.post("http://localhost:3001/shops",qs.stringify({shop: {shop_name: "aaaaaaa"}}))
+        const params = qs.stringify({shop: {
+                                            shop_name: data.shopName,
+                                            address_number: data.AddressNumber,
+                                            prefecture: data.prefecture,
+                                            city: data.city,
+                                            block: data.block,
+                                            building: data.building
+                                        }})
+        axios.post("http://localhost:3001/shops",params)
 
         .then(response => {
             const data = response.data;
@@ -17,18 +26,31 @@ const NewShopComponent = () => {
             console.log(data);
         })
     }
-    return (
-        <div>
-            店名<input type="text"></input>
-            住所<input type="text"></input>
-            営業日<input type="text"></input>
-            定休日<input type="text"></input>
 
-            <input 
-            type="submit"
-            onClick={() => submit()}
-            ></input>
-        </div>
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                {errors.shopName && "店名の入力は必須です。"}<br />
+                店名　　　<input type="text" name="shopName" ref={register({ required: true })} /><br />
+                
+                {errors.AddressNumber && "郵便番号の入力は必須です。"}<br />
+                郵便番号　<input type="text" name="AddressNumber" ref={register({ required: true })} /><br />
+                
+                {errors.prefecture && "都道府県の入力は必須です。"}<br />
+                都道府県　<input type="text"name="prefecture" ref={register({ required: true })} /><br />
+                
+                {errors.city && "市町村の入力は必須です。"}<br />
+                市町村　　<input type="text" name="city" ref={register({ required: true })} /><br />
+                
+                {errors.block && "番地の入力は必須です。"}<br />
+                番地　　　<input type="text" name="block" ref={register({ required: true })} /><br />
+                
+                <br />
+                建物　　　<input type="text" name="building" ref={register({ required: true })} /><br />
+
+                <input type="submit"/>
+            </form>
+        </>
     )
 }
 
